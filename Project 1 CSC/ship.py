8e112fd209141e7eee3a9139143764978f65c3a5
SujalK96 <159101868+SujalK96@users.noreply.github.com>
2024-04-23 14:21:59 -0400

    Update ship.py

--- Project 1 CSC/ship.py
+++ Project 1 CSC/ship.py
@@ -1,20 +1,16 @@
 import pygame
-
-from pygame.sprite import Sprite
-
-
-class Ship(Sprite):
+ 
+class Ship:
     """A class to manage the ship."""
-
+ 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
-        super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('ship.bmp')
+        self.image = pygame.image.load('/Users/sujalkarki/Desktop/ProjectOne(1)/ship.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
@@ -41,8 +37,3 @@ class Ship(Sprite):
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
-
-    def center_ship(self):
-        """Center the ship on the screen."""
-        self.rect.midbottom = self.screen_rect.midbottom
-        self.x = float(self.rect.x)
